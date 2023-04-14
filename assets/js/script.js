adb12256ab5b410b3bbe48fbf19576e5427c2413
Drew <drewkretschmar@yahoo.com>
2023-04-13 20:45:49 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,27 +20,48 @@ setInterval(time,1000);
 
 function runForecast() {
     var forecast = document.getElementById('forecast');
+    var current = document.getElementById('current');
     var city = document.getElementById('input').value;
     console.log(city);
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q='+ city +'&appid=f1e9804071ae403822444ebea900347d&units=imperial';
-    fetch(requestUrl)
+    var fiveDayForecast = 'https://api.openweathermap.org/data/2.5/forecast?q='+ city +'&appid=f1e9804071ae403822444ebea900347d&units=imperial';
+    var currentWeather = 'https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=f1e9804071ae403822444ebea900347d&units=imperial';
+    fetch(currentWeather)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        var searchedCity = document.getElementById('cityname');
+        var temp = document.getElementById('temp');
+        var wind = document.getElementById('wind');
+        var humidity = document.getElementById('humidity');
+
+        searchedCity.textContent = data.name;
+        temp.textContent = 'Temp: '+data.main.temp+'°F';
+        wind.textContent = 'Wind: '+data.wind.speed+' mph';
+        humidity.textContent = 'Humidity: '+data.main.humidity+'%';
+    });
+    fetch(fiveDayForecast)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data)
-            for (var i=0; i<1; i++) {
+            for (var i=0; i<5; i++) {
                 var dayForecast = document.createElement('div');
+                dayForecast.className = 'forecast';
+                var searchedCity = document.createElement('p');
                 var date = document.createElement('p');
                 var temp = document.createElement('p');
                 var wind = document.createElement('p');
                 var humidity = document.createElement('p');
 
-                date.textContent = data[list.i.dt];
-                temp.textContent = 'Temp: '+list.data[i].main.temp;
-                wind.textContent = 'Wind: '+list.data[i].wind.speed;
-                humidity.textContent = 'Humidity: '+list.data[i].main.humidity;
-
+                date.textContent = 'Date: '//+data.main.dt_txt;
+                temp.textContent = 'Temp: '//+data.main.temp;
+                wind.textContent = 'Wind: '//+data.wind.speed;
+                humidity.textContent = 'Humidity: '//+data.main.humidity;
+                
+                dayForecast.appendChild(searchedCity);
                 dayForecast.appendChild(date);
                 dayForecast.appendChild(temp);
                 dayForecast.appendChild(wind);
